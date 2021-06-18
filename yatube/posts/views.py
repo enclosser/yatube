2acda5c71a5f2416a3df2c97b20eb5f67a1284e7
andrey <66161872+Andrey-oss-ai@users.noreply.github.com>
2021-06-18 21:14:55 +0300

    Startap project

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Post
 
-
 def index(request):
-# одна строка вместо тысячи слов на SQL
-    latest = Post.objects.order_by('-pub_date')[:10]
-    # собираем тексты постов в один, разделяя новой строкой
-    output = []
-    for item in latest:
-        output.append(item.text)
-    return HttpResponse('\n'.join(output))
+    latest = Post.objects.order_by("-pub_date")[:11]
+    return render(request, "index.html", {"posts": latest})
